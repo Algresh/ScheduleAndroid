c861da4e793ca932cceb904debd43da81ba6e8a3
Algresh <tulupov.alex@gmail.com>
2016-03-11 22:17:13 +0300

    add toolBar

--- app/src/main/java/com/example/alex/scheduleandroid/MainActivity.java
+++ app/src/main/java/com/example/alex/scheduleandroid/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.alex.scheduleandroid;
 
+import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 import com.example.alex.scheduleandroid.adapter.GroupListAdapter;
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
 
     private ConnectedManager connectedManager;
+    private Toolbar toolbar;
 
 
     @Override
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
         initNavigationView();
+        initToolBar();
 
         connectedManager = new ConnectedManager(this);
 
@@ -39,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycleView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new GroupListAdapter(createMockDepartmentListData() , this));
+
+    }
+
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+//        toolbar.inflateMenu(R.menu.menu);
 
     }
 
@@ -57,6 +78,31 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this , drawerLayout , toolbar , R.string.open , R.string.close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.listOfGroups:
+                        /**
+                         * @TODO Проверку в какой активити ты находишься
+                         */
+
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 
